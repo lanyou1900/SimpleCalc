4e9a529c3b79d519ab02e4fcc1ca50b6ad20d325
Niels Kristian Hansen Skovmand <niels@skovmand.org>
2012-02-04 14:33:46 +0100

    Version 1.10. Completely new parser. Loving the java language!

--- README.txt
+++ README.txt
@@ -1,17 +1,31 @@
 This is my Simple Calculator. A silly and simple project to learn Java coding and Github usage.
 The calculator was initially an integer calculator, but changed using the BigDecimal-class. So now it also handles floats, doubles, etc.
 
-Usage:
-"java SimpleCalc number1 (+-*/) number2."
-You might have to use \* instead of * because the Terminal interprets * as a wildcard.
+Usage: "java SimpleCalc [math expression] (precision)"
+   - [math expression] is the mathematical expression such as "98*4+2.2"
+   - (precision) is the number of digits you would like to get in the final result. Default is 10.
+     All precision during the calculations are done with precision*100. Only the last result is rounded to the value of precision.
+
 
 Changelog:
 
+Version 1.10:
+- completely new parser
+- new usage: "java SimpleCalc [math expression] (precision)"
+   - [math expression] is the mathematical expression such as "98*4+2.2"
+   - (precision) is the number of digits you would like to get in the final result. Default is 10.
+     All precision during the calculations is precision*100. Only the last result is rounded to the value of precision.
+- one bug persists: you can't use "/-" in the calculation, for example "2/-2" will crash. Use "-2/2" instead.
+  I will fix it ASAP.
+- another limitation: no parenthesises are allowed yet, nor are sinus, pi, etc...
+
 Version 1.03:
+- usage: "java SimpleCalc number1 (+-*/) number2."
 - better source code - simplified by Daniel
 - same functionality
 
 Version 1.02:
+- usage: "java SimpleCalc number1 (+-*/) number2."
 - fixed basic bug (crashed when no args was used)
 - better error handling when numbers are invalid.
 
--- src/SimpleCalc.java
+++ src/SimpleCalc.java
@@ -13,65 +13,37 @@
  */
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class SimpleCalc {
     
     // Initialise public version number and usage info.
-    public static String welcomeMsg = "SimpleCalc - The simple calculator - v1.03";
-    public static String usage = "Usage: SimpleCalc number1 [+-*/] number2. To multiply, use \\* instead of *.";
-    
-    // Initialise variables to be used in calculations.
-    public static BigDecimal number1bd = BigDecimal.ZERO;
-    public static BigDecimal number2bd = BigDecimal.ZERO;
-    
-    // Initialise plain string-values for output (to make the code simpler later...)
-    public static String number1s = "";
-    public static String number2s = "";
+    public static String welcomeMsg = "SimpleCalc - The simple calculator - v1.1";
+    public static String usage = "Usage: SimpleCalc [math expression] (precision). Use only +, -, * and /.";
+
+    // Default Precision Variable
+    public static Integer outputPrecision = 10;
     
     
     public static void fatalError(String exitMsg, int errorCode) {
-        System.out.println("Fatal Error " + errorCode + ": " + exitMsg);
+        if (exitMsg.length() > 0)   // if the exitMsg is not ""
+            System.out.println("Fatal Error " + errorCode + ": " + exitMsg);
+
         System.out.println(usage);
+        System.out.println("");
+
         System.exit(errorCode);  
     }
-    
-    private static int initialiseModeOfOperation(String[] args) {
-        // Check for arguments.
-        if (args.length < 3)
-            fatalError("Not enough arguments given.", 1);
 
-        // The first and third argument should be a number, the second a single char.
-        // What is the mode of operation?
-        int modeOfOperation = 0;
-        
-        if (args[1].equalsIgnoreCase("+"))
-            modeOfOperation=1;
-        else if (args[1].equalsIgnoreCase("-"))
-            modeOfOperation=2;
-        else if (args[1].equalsIgnoreCase("*"))
-            modeOfOperation=3;
-        else if (args[1].equalsIgnoreCase("/"))
-            modeOfOperation=4;
-        else
-            fatalError("Invalid operator '" + args[1] + "'. Use +, -, * or /", 1);
-        
-        return modeOfOperation;
-    }
-    
-    private static void initialiseNumbers(String[] args) {
-        // The first and third argument should be a number, the second a single char.
-        
-        // Are the two numbers actually numbers?
-        // Initialise the two numbers into number1bd and number2bd:
-        
-        // Initialise number1bd and number2bd from args[0] and args[2].
-        number1bd = toBigDecimal(args[0]).stripTrailingZeros();
-        number2bd = toBigDecimal(args[2]).stripTrailingZeros();
-        
-        // Create plain text versions of BigDecimals
-        number1s = number1bd.toPlainString();
-        number2s = number2bd.toPlainString();
-        
+    private static void initialiseArgumentsAndSetPrecision(String[] args) {
+        // Check for arguments.
+        if (args.length < 1 || args[0].equals("?") || args[0].equals("--help"))
+            fatalError("", 1);
+
+        if(args.length > 1 && isInteger(args[1]))
+            outputPrecision = new Integer(args[1]);
+        else if(args.length > 1 && !isInteger(args[1]))
+            fatalError("Invalid precision. Must be an integer.",1);
     }
     
     private static BigDecimal toBigDecimal(String num) throws NumberFormatException {
@@ -83,72 +55,160 @@ public class SimpleCalc {
             fatalError("Invalid number '" + num + "'. Must be a numeric value. Use '.' for decimals.", 2);
         }
         
-        return BigDecimal.ZERO;
+        return BigDecimal.ZERO; // is never called.
     }
-    
-    
-    // Functions for doing basic calculations:
-    private static String addTwoNumbers(BigDecimal num1, BigDecimal num2) {
-        return num1.add(num2)
-                   .stripTrailingZeros()
-                   .toPlainString();
-    }
-    
-    private static String subtractTwoNumbers(BigDecimal num1, BigDecimal num2) {
-        return num1.subtract(num2)
-                   .stripTrailingZeros()
-                   .toPlainString();
-    }
-    
-    private static String multiplyTwoNumbers(BigDecimal num1, BigDecimal num2) {
-        return num1.multiply(num2)
-                   .stripTrailingZeros()
-                   .toPlainString();
-    }
-    
-    private static String divideTwoNumbers(BigDecimal num1, BigDecimal num2) {
-        return num1.divide(num2, 10, BigDecimal.ROUND_HALF_UP)
-                   .stripTrailingZeros()
-                   .toPlainString();
-    }
-    
-    
-    public static void main(String args[]) {
-        
-        System.out.println(welcomeMsg);
-        
-        // Get mode of operation and numbers from command line.
-        int modeOfOperation = initialiseModeOfOperation(args);
-        initialiseNumbers(args);
-        
-        // Initiate result-variable as a string.
-        String result;
-        
-        // Do the math!
-        // checkArguments(args) returns an int with the mode of operation or a fatal error, if the arguments are invalid.
-        
-        switch ( modeOfOperation )
-        {
-            case 1:
-                result = addTwoNumbers(number1bd,number2bd);
-                System.out.println(number1s + " added to " + number2s + " equals " + result);
-                break;
-            case 2:
-                result = subtractTwoNumbers(number1bd,number2bd);
-                System.out.println(number2s + " subtracted from " + number1s + " equals " + result);
-                break;
-            case 3:
-                result = multiplyTwoNumbers(number1bd,number2bd);
-                System.out.println(number1s + " multiplied by " + number2s + " equals " + result);
-                break;
-            case 4:
-                result = divideTwoNumbers(number1bd,number2bd);
-                System.out.println(number1s + " divided by " + number2s + " equals " + result);
-                
-                break;
+
+    private static boolean isBigDecimal(String num) {
+        // Test if a number is a BigDecimal.
+
+        BigDecimal testVariable = BigDecimal.ZERO;
+
+        try {
+            testVariable = new BigDecimal(num);
+        } catch (NumberFormatException e) {
+            return false;
         }
+
+        return true;
+    }
+
+    private static boolean isInteger(String num) {
+        // Test if a number is an integer..
+
+        Integer testVariable = 0;
+
+        try {
+            testVariable = new Integer(num);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    private static String [] splitStringAtPlusSigns(String input) {
+        // Replace '-' with '+-' to be able to split the string at '+' for both subtraction and addition.
+        // Afterwards, split the array at '+'
+
+        String replacedPlusMinusString = "";
+
+        if ( input.substring(0,1).equals("-") ) {
+            // If first char is "-", it shouldn't be replaced (or else the string with start like "+-2+2" and the program will crash).
+            replacedPlusMinusString = "-" + input.substring(1).replaceAll("-","+-");
+        }
+        else {
+            replacedPlusMinusString = input.replaceAll("-","+-");
+        }
+
+        // Split string at '+'
+        return replacedPlusMinusString.split("\\+");
+    }
+    
+    private static String [] splitStringAtMultiplicationDivisionSigns(String input) {
+
+        // Replace the "/" with "*1/" - to turn the division into multiplication.
+        String replacedMultiplicationDivisionString = input.replaceAll("/","*1/");
+
+        // Split the string at "*"
+        return replacedMultiplicationDivisionString.split("\\*");
+    }
+
+    private static String calculateNumericValue (String input) {
+        // Try to calculate the numeric value of the input string.
+        // For now, assume that it contains only multiplication and division.
+
+        // Replace the "/" with "*1/" - to turn the division into multiplication.
+        // Split the string at "*"
+        String [] splitArrayAtStar = splitStringAtMultiplicationDivisionSigns(input);
+
+        // Initiate tempMultiplicationValue as splitArrayAtStarOrDash[0] (to get an initial value for multiplication).
+        BigDecimal tempMultiplicationValue = new BigDecimal(splitArrayAtStar[0]);
+
+        // Cycle through the array split at '*' and calculate the product.
+        // Notice the loop starts at j=1 - that is because tempMultiplicationValue is set as splitArrayAtStar[0].
+        for (int j=1 ; j<splitArrayAtStar.length ; j++)
+        {
+
+            // Check if the value is a BigDecimal. If not, assume that it is because it contains "/" (dashes) and calculate the division.
+            if (!isBigDecimal(splitArrayAtStar[j]))
+            {
+                // Split the number at "/"
+                String [] splitArrayAtDash = splitArrayAtStar[j].split("/");
+
+                // Initiate temporary BigDecimal value as first array value.
+                BigDecimal tempDivisionValue = new BigDecimal(splitArrayAtDash[0]);
+
+                // Initiate the second value as a BigDecimal.
+                BigDecimal tempStringAsBigDecimal = new BigDecimal(splitArrayAtDash[1]);
+
+                // Do the division. As all the divisions are done as "1/x", there are always only two values here.
+                tempDivisionValue = tempDivisionValue.divide(tempStringAsBigDecimal, outputPrecision*100, BigDecimal.ROUND_HALF_UP);
+
+                // Put the value back in the splitArrayAtStar[j].
+                splitArrayAtStar[j] = tempDivisionValue.toPlainString();
+            }
+
+            // Turn the value at splitArrayAtStar[j] into a BigDecimal.
+            BigDecimal tempStringAsBigDecimal = new BigDecimal(splitArrayAtStar[j]);
+
+            // As the loops iterates, tempMultiplicationValue will be the product of the array values.
+            tempMultiplicationValue = tempMultiplicationValue.multiply(tempStringAsBigDecimal);
+        }
+
+        // Now the product of the splitArrayAtPlus-array value is calculated. Put it back into the array.
+        return tempMultiplicationValue.toPlainString();
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println(welcomeMsg);
+        initialiseArgumentsAndSetPrecision(args);
+
+        System.out.println("Precision is " + outputPrecision + " decimals.");
+
+
+        // Parse the input string.
+
+        // 1:
+        // Replace '-' with '+-' to be able to split the string at '+' for both subtraction and addition.
+        // Afterwards, split the array at '+'
+        // The result is an array with values to be added together.
+        String [] splitArrayAtPlus = splitStringAtPlusSigns(args[0]);
+
         
-        // Print an extra line.
+        // 2:
+        // Calculate the numeric value of each entry in the splitArrayAtPlus-array.
+        // When all entries are numeric, the sum can be calculated.
+
+        for (int i=0 ; i<splitArrayAtPlus.length ; i++)
+        {
+            // Two scenarios:
+            // 1: Either the string is a BigDecimal (if there are no "/" and "*"-signs in the string)
+            // 2: The string contains contains non-numeric chars.
+
+            // Use boolean isBigDecimal to check if the number is a BigDecimal.
+            // If it is not a BigDecimal (ie. a numeric value), try to calculate the numeric value.
+
+            if ( !isBigDecimal(splitArrayAtPlus[i]) )
+                splitArrayAtPlus[i] = calculateNumericValue( splitArrayAtPlus[i] );
+        }
+
+
+        // Calculate the sum of all values in splitArrayAtPlus:
+
+        BigDecimal finalSum = BigDecimal.ZERO;
+
+        for (int i = 0 ; i<splitArrayAtPlus.length ; i++)
+            finalSum = finalSum.add( toBigDecimal(splitArrayAtPlus[i]) );
+
+        // Divide with one and round to outputPrecision to get the final result
+        finalSum = finalSum.divide(BigDecimal.ONE, outputPrecision, BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
+        
+        // Print the result.
+        System.out.println(args[0] + " = " + finalSum.toPlainString());
+
+        // Print an extra line...
         System.out.println();
         
     }
